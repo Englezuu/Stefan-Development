2b36ffa99c99964627127f26c894395f6d6bad9a
13.stefannnnn <149679286+Englezuu@users.noreply.github.com>
2025-03-15 19:49:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-import { FutureJS } from './future.js';
+const { FutureJS } = require('./future.js');
+
+
 
 // Use FutureJS as needed
 FutureJS.RegisterHTMLVar({ id: "Textt", value: "This is a new text" });
